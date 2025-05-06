7eae8960dba280e4b7c55e1eb3ad0027ea3e43ec
Anibal lucero <llucerob@gmail.com>
2025-05-05 21:38:59 -0400

    version antigua

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -2,49 +2,8 @@
 <html lang="en" >
 
 <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description"
-            content="Cuba admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
-        <meta name="keywords"
-            content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
-        <meta name="author" content="pixelstrap">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
-        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
-        <title>@yield('title') | Cuba - Premium Admin Template By Pixelstrap</title>
-        <!-- Google font-->
-        <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap"
-            rel="stylesheet">
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <script>
-            var baseUrl = "{{ asset('') }}";
-        </script>
-        <!-- Font Awesome-->
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/fontawesome.css') }}">
-        <!-- ico-font-->
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/icofont.css') }}">
-        <!-- Themify icon-->
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/themify.css') }}">
-        <!-- Flag icon-->
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/flag-icon.css') }}">
-        <!-- Feather icon-->
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/feather-icon.css') }}">
-        <!-- Plugins css start-->
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/slick.css') }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/slick-theme.css') }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/scrollbar.css') }}">
-        <!-- Plugins css Ends-->
-        @yield('css')
-        <!-- Bootstrap css-->
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/bootstrap.css') }}">
-        <!-- App css-->
-        @vite(['public/assets/scss/style.scss'])
-        <link id="color" rel="stylesheet" href="{{ asset('assets/css/color-1.css') }}" media="screen">
-        <!-- Responsive css-->
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/responsive.css') }}">
+    @include('layout.head')
+    @include('layout.css')
 </head>
 
 
@@ -86,51 +45,15 @@
                 
             </div>
 
-            <footer class="footer"> 
-
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-12 footer-copyright text-center">
-                            <p class="mb-0">Copyright <span class="year-update"> </span> © Luis Lucero B. </p>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+            @include('layout.footer')
   
         </div>
 
         
     </div>
     
-    <!-- latest jquery-->
-    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
-
-    <!-- feather icon js-->
-    <script src="{{ asset('assets/js/icons/feather-icon/feather.min.js') }}"></script>
-    <script src="{{ asset('assets/js/icons/feather-icon/feather-icon.js') }}"></script>
-    <!-- scrollbar js-->
-    <script src="{{ asset('assets/js/scrollbar/simplebar.min.js') }}"></script>
-    <script src="{{ asset('assets/js/scrollbar/custom.js') }}"></script>
-    <!-- Sidebar jquery-->
-    <script src="{{ asset('assets/js/config.js') }}"></script>
-    <!-- Plugins JS start-->
-    <script src="{{ asset('assets/js/sidebar-menu.js') }}"></script>
-    <script src="{{ asset('assets/js/sidebar-pin.js') }}"></script>
-    <script src="{{ asset('assets/js/slick/slick.min.js') }}"></script>
-
-    <script src="{{ asset('assets/js/header-slick.js') }}"></script>
-    @yield('scripts')
-    
-    <script src="{{ asset('assets/js/script.js') }}"></script>
-
-
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            if (window.flasher) {
-                window.flasher.render();
-            }
-        });
-    </script>
+    @include('layout.scripts')
+   
     
   
 </body>
